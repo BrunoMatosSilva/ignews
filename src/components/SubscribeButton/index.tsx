ecd537f19982c50f062ecce990ad0d3bb91c2f6f
BrunoMatosSilva <brunomatos2010@outlook.com>
2021-04-09 17:06:58 -0300

    first commit

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './styles.module.scss';
+
+interface SubscribeButtonProps {
+    priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    return (
+        <button
+            type="button"
+            className={styles.subscribeButton}
+        >
+            Subscribe Now
+        </button>
+    )
+}
